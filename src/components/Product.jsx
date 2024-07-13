265a6cfa44dba4405d97711412cae75a5b299953
Taras38 <122573790+Taras38@users.noreply.github.com>
2024-07-13 14:03:24 +0300

    [12]

---
+++ src/components/Product.jsx
@@ -0,0 +1,13 @@
+// src/components/Product.jsx
+
+export const Product = () => {
+	const price = 999;
+
+  return (
+    <>
+      <h2>Tacos</h2>
+			<img src="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>" alt="Tacos With Lime" width="640" />
+      <p>Price: {price} credits</p>
+    </>
+  );
+};
